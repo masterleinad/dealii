96d2961b3d81c00d2792bb02e84ef50a1a8d1375
Daniel Arndt <arndtd@ornl.gov>
2023-02-13 14:54:34 -0500

    Request verbose makefiles

--- cmake/macros/macro_check_compiler_setup.cmake
+++ cmake/macros/macro_check_compiler_setup.cmake
@@ -51,12 +51,17 @@ macro(check_compiler_setup _compiler_flags_unstr _linker_flags_unstr _var)
     )
   set(CACHED_${_var}_ARGN "${ARGN}" CACHE INTERNAL "" FORCE)
 
+  message(STATUS "Testing ${_var}") 
+
   try_compile(
     ${_var}
     ${CMAKE_CURRENT_BINARY_DIR}/check_compiler_setup/CheckCompilerSetup${_var}
     ${CMAKE_CURRENT_SOURCE_DIR}/cmake/macros/check_compiler_setup
     CheckCompilerSetup${_var}
-    CMAKE_FLAGS "-DTEST_COMPILE_FLAGS=${_compiler_flags}" "-DTEST_LINKER_FLAGS=${_linker_flags}" "-DTEST_LINK_LIBRARIES=${ARGN}"
+    CMAKE_FLAGS "-DTEST_COMPILE_FLAGS=${_compiler_flags}" 
+                "-DTEST_LINKER_FLAGS=${_linker_flags}"
+                "-DTEST_LINK_LIBRARIES=${ARGN}"
+                "-DCMAKE_VERBOSE_MAKEFILE=ON"
     OUTPUT_VARIABLE _output)
 
   message(STATUS "Compiler setup check for ${_var} failed with:\n${_output}")
