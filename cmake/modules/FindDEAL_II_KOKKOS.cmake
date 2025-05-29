d5aa3b13df142c90faa2a378795c1bd35bcdcb02
Daniel Arndt <arndtd@ornl.gov>
2025-05-29 11:00:58 -0400

    Drop Kokkos minimum supported version to 3.4

--- cmake/modules/FindDEAL_II_KOKKOS.cmake
+++ cmake/modules/FindDEAL_II_KOKKOS.cmake
@@ -33,7 +33,7 @@ set_if_empty(KOKKOS_DIR "$ENV{KOKKOS_DIR}")
 set(CMAKE_CXX_EXTENSIONS OFF)
 
 # The minimal Kokkos version we support
-set(KOKKOS_MINIMUM_REQUIRED_VERSION 3.7.0)
+set(KOKKOS_MINIMUM_REQUIRED_VERSION 3.4)
 
 #
 # Make sure that we prioritize Kokkos bundled with Trilinos or PETSc.
